28dfa5b51e78d0cf4941676d65436f6d605d1a8b
apple <754090288@qq.com>
2018-12-12 19:26:56 +0800

    Initial commit

---
+++ src/main/java/com/mrzhang/test.java
@@ -0,0 +1,12 @@
+package com.mrzhang;
+
+/**
+ * @author shuaizhang
+ * @mail zs951027@126.com
+ * @date 18/12/12 下午7:24
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello word !!");
+    }
+}
